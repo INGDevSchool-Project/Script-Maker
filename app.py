294fb3ce3ec21f4aaff7c1b103cdb5f35645d3cc
RaduFilip16 <filip16radu.m@gmail.com>
2022-05-19 01:43:37 +0300

    Add os variable that simplifies the code

--- app.py
+++ app.py
@@ -50,106 +50,18 @@ def result():
     model=["Create directory at [path]/[directory name]","Create empty file at [path]/[file.ext]","Delete directory from [path]/[directory name]","Delete file from [path]/[file.ext]","Move file [file.ext] to [path]","Rename file [file.ext] to [newfile.ext]","Replace text [your-text] with [new-text] in file [file.ext]","Delete empty lines from [path]/[file.ext]","Print lines matching [pattern] from [path]/[file.ext]"]
     option = request.form['options']
     if (option == "linux"):
-        corect = 0
-        for i in subcereri:
-
-            ## Pentru model cu comenzi in baza de date
-            ## Pentru fiecare comanda, cererea o sa arate intr-un anumit fel
-            ## Ex: Create a directory: Create directory at [path]/[directory name]
-            ## In baza de date putem avea o coloana cu comanda in sine si un identificator, spre ex. primele 2 cuvinte din cerere
-            ## Codu de mai jos o sa se schimbe intr-un name.append(*interogare baza de date pe baza primelor 2 cuvinte din cerere* + ...)
-            ## Poate o sa mai fie un if sau poate mai multe, s-ar putea sa fie niste cazuri speciale
-            impartire = i.split(" ")
-            if len(impartire) == 4:
-                if impartire[2] != "at" and impartire[2] != "from":
-                    flash("The request was incorrect!", "warning")
-                    redirect(url_for("home"))
-                    return render_template("index.html")
-            else:
-                if len(impartire) == 5:
-                    if impartire[0] == "Create" and impartire[1] == "empty":
-                        if impartire[2] != "file" or impartire[3] != "at":
-                            flash("The request was incorrect!", "warning")
-                            redirect(url_for("home"))
-                            return render_template("index.html")
-                    if impartire[0] == "Move" and impartire[1] == "file":
-                        if impartire[3] != "to":
-                            flash("The request was incorrect!", "warning")
-                            redirect(url_for("home"))
-                            return render_template("index.html")
-                    if impartire[0] == "Rename" and impartire[1] == "file":
-                        if impartire[3] != "to":
-                            flash("The request was incorrect!", "warning")
-                            redirect(url_for("home"))
-                            return render_template("index.html")
-                    if impartire[0] == "Delete" and impartire[1] == "empty":
-                        if impartire[2] != "lines" or impartire[3]!="from":
-                            flash("The request was incorrect!", "warning")
-                            redirect(url_for("home"))
-                            return render_template("index.html")
-                else:
-                    if len(impartire) == 6:
-                        if impartire[0] == "Print" and impartire[1] == "lines":
-                            if impartire[2] != "matching" or impartire[4] != "from":
-                                flash("The request was incorrect!", "warning")
-                                redirect(url_for("home"))
-                                return render_template("index.html")
-                    else:
-                        if len(impartire) == 8:
-                            if impartire[0] == "Replace" and impartire[1] == "text":
-                                if impartire[3] != "with" or impartire[5] != "in" or impartire[6]!= "file":
-                                    flash("The request was incorrect!", "warning")
-                                    redirect(url_for("home"))
-                                    return render_template("index.html")
-                        else:
-                            flash("The request was incorrect!", "warning")
-                            redirect(url_for("home"))
-                            return render_template("index.html")
-
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Create directory"):
-                name.append(tables[0][3] + " " + (i.split(" at ", 1)[1]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Create empty"):
-                name.append(tables[1][3] + " " + (i.split(" at ", 1)[1]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Delete directory"):
-                name.append(tables[2][3] + " " + (i.split(" from ", 1)[1]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Delete file"):
-                name.append(tables[3][3] + " " + (i.split(" from ", 1)[1]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Move file"):
-                name.append(tables[4][3] + " " + (i.split(" ", 4)[2]) + " " + (i.split(" ", 4)[4]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Rename file"):
-                name.append(tables[5][3] + " " + (i.split(" ", 4)[2]) + " " + (i.split(" ", 4)[4]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Replace text"):
-                name.append(tables[6][3] + " " + (i.split(" ", 7)[7]) + ") " + "-replace " + "'" + (
-                i.split(" ", 7)[2]) + "'" + ", " + "'" + (i.split(" ", 7)[4]) + "'" + "|" + "Out-File " + (
-                            i.split(" ", 7)[7]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Delete empty"):
-                name.append(
-                    tables[7][3] + " " + (i.split(" ", 4)[4]) + ") | ? {$_.trim() -ne ""} | set-content " + (
-                    i.split(" ", 4)[4]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Print lines"):
-                name.append(tables[8][3] + " " + i.split(" ", 5)[3] + " " + i.split(" ", 5)[5])
-                corect += 1
-
-        if corect != len(subcereri):
-            flash("The request was incorrect!", "warning")
-            redirect(url_for("home"))
-            return render_template("index.html")
-        else:
-            length = len(name)
-            session['script'] = name
-            session['os'] = option
-            return render_template("index.html", name = name, length = length)
-
+        os = 2
     else:
-        corect = 0
+        os = 3
+    corect = 0
+    for i in subcereri:
+
+        ## Pentru model cu comenzi in baza de date
+        ## Pentru fiecare comanda, cererea o sa arate intr-un anumit fel
+        ## Ex: Create a directory: Create directory at [path]/[directory name]
+        ## In baza de date putem avea o coloana cu comanda in sine si un identificator, spre ex. primele 2 cuvinte din cerere
+        ## Codu de mai jos o sa se schimbe intr-un name.append(*interogare baza de date pe baza primelor 2 cuvinte din cerere* + ...)
+        ## Poate o sa mai fie un if sau poate mai multe, s-ar putea sa fie niste cazuri speciale
         impartire = i.split(" ")
         if len(impartire) == 4:
             if impartire[2] != "at" and impartire[2] != "from":
@@ -174,7 +86,7 @@ def result():
                         redirect(url_for("home"))
                         return render_template("index.html")
                 if impartire[0] == "Delete" and impartire[1] == "empty":
-                    if impartire[2] != "lines" or impartire[3] != "from":
+                    if impartire[2] != "lines" or impartire[3]!="from":
                         flash("The request was incorrect!", "warning")
                         redirect(url_for("home"))
                         return render_template("index.html")
@@ -188,7 +100,7 @@ def result():
                 else:
                     if len(impartire) == 8:
                         if impartire[0] == "Replace" and impartire[1] == "text":
-                            if impartire[3] != "with" or impartire[5] != "in" or impartire[6] != "file":
+                            if impartire[3] != "with" or impartire[5] != "in" or impartire[6]!= "file":
                                 flash("The request was incorrect!", "warning")
                                 redirect(url_for("home"))
                                 return render_template("index.html")
@@ -197,47 +109,49 @@ def result():
                         redirect(url_for("home"))
                         return render_template("index.html")
 
-        for i in subcereri:
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Create directory"):
-                name.append(tables[0][3] + " " + (i.split(" at ", 1)[1]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Create empty"):
-                name.append(tables[1][3] + " " + (i.split(" at ", 1)[1]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Delete directory"):
-                name.append(tables[2][3] + " " + (i.split(" from ", 1)[1]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Delete file"):
-                name.append(tables[3][3] + " " + (i.split(" from ", 1)[1]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Move file"):
-                name.append(tables[4][3] + " " + (i.split(" ", 4)[2]) + " " + (i.split(" ", 4)[4]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Rename file"):
-                name.append(tables[5][3] + " " + (i.split(" ", 4)[2]) + " " + (i.split(" ", 4)[4]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Replace text"):
-                name.append(tables[6][3] + " " + (i.split(" ", 7)[7]) + ") " + "-replace " + "'" + (
-                i.split(" ", 7)[2]) + "'" + ", " + "'" + (i.split(" ", 7)[4]) + "'" + "|" + "Out-File " + (
-                            i.split(" ", 7)[7]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Delete empty"):
-                name.append(tables[7][3] + " " + (i.split(" ", 4)[4]) + ") | ? {$_.trim() -ne ""} | set-content " + (
+        if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Create directory"):
+            name.append(tables[0][os] + " " + (i.split(" at ", 1)[1]))
+            corect += 1
+        if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Create empty"):
+            name.append(tables[1][os] + " " + (i.split(" at ", 1)[1]))
+            corect += 1
+        if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Delete directory"):
+            name.append(tables[2][os] + " " + (i.split(" from ", 1)[1]))
+            corect += 1
+        if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Delete file"):
+            name.append(tables[3][os] + " " + (i.split(" from ", 1)[1]))
+            corect += 1
+        if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Move file"):
+            name.append(tables[4][os] + " " + (i.split(" ", 4)[2]) + " " + (i.split(" ", 4)[4]))
+            corect += 1
+        if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Rename file"):
+            name.append(tables[5][os] + " " + (i.split(" ", 4)[2]) + " " + (i.split(" ", 4)[4]))
+            corect += 1
+        if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Replace text"):
+            name.append(tables[6][os] + " " + (i.split(" ", 7)[7]) + ") " + "-replace " + "'" + (
+            i.split(" ", 7)[2]) + "'" + ", " + "'" + (i.split(" ", 7)[4]) + "'" + "|" + "Out-File " + (
+                        i.split(" ", 7)[7]))
+            corect += 1
+        if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Delete empty"):
+            name.append(
+                tables[7][3] + " " + (i.split(" ", 4)[4]) + ") | ? {$_.trim() -ne ""} | set-content " + (
                 i.split(" ", 4)[4]))
-                corect += 1
-            if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Print lines"):
-                name.append(tables[8][3] + " " + i.split(" ", 5)[3] + " " + i.split(" ", 5)[5])
-                corect += 1
+            corect += 1
+        if ((i.split(" ")[0] + " " + i.split(" ")[1]) == "Print lines"):
+            name.append(tables[8][3] + " " + i.split(" ", 5)[3] + " " + i.split(" ", 5)[5])
+            corect += 1
 
-        if corect != len(subcereri):
-            flash("The request was incorrect!", "warning")
-            redirect(url_for("home"))
-            return render_template("index.html")
-        else:
-            length = len(name)
-            session['script'] = name
-            session['os'] = option
-            return render_template("index.html", name = name, length = length)
+    if corect != len(subcereri):
+        flash("The request was incorrect!", "warning")
+        redirect(url_for("home"))
+        return render_template("index.html")
+    else:
+        length = len(name)
+        session['script'] = name
+        session['os'] = option
+        return render_template("index.html", name = name, length = length)
+
+
 
 @app.route("/download", methods = ['POST', 'GET'])
 def download():
